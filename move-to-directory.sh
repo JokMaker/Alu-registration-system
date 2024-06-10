4c060fe1d50fe837946768222fac9ad327998107
Innocent-Gershon <i.nangah@alustudent.com>
2024-06-10 13:49:37 -0700

    File created

---
+++ move-to-directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIRECTORY="negpod_id-q1"
+
+# Ensure the directory exists
+mkdir -p $DIRECTORY
+
+# Move files to the directory
+mv main.sh students-list_1023.txt select-emails.sh student-emails.txt $DIRECTORY
+
+echo "Files moved to $DIRECTORY"
