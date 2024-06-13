876d073dbe1505a99f3b92991f58cc7817f370d9
JokMaker <j.kur@alustudent.com>
2024-06-12 17:57:28 -0700

    Done

--- negpod_id-q1/select-emails.sh
+++ negpod_id-q1/select-emails.sh
@@ -4,8 +4,9 @@ STUDENT_FILE="students-list_0524.txt"
 EMAIL_FILE="student-emails.txt"
 
 if [ -f $STUDENT_FILE ]; then
-    cut -d',' -f1 $STUDENT_FILE > $EMAIL_FILE
-    echo "Emails extracted to $EMAIL_FILE"
+    awk -F', ' '{print $1}' $STUDENT_FILE | sort > $EMAIL_FILE
+    echo "Student emails saved in ascending order to $EMAIL_FILE."
 else
     echo "No student records found."
 fi
+
